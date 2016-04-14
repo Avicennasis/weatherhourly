cb42d1f566215fcabbbe3fe88a0558287ec4b6d9
Avic <Avicennasis@gmail.com>
2016-04-14 07:10:19 -0400

    Update weatherhourly.sh
    link update

--- weatherhourly.sh
+++ weatherhourly.sh
@@ -2,6 +2,6 @@
 
 echo "fetching your local hourly weather report"
 sleep 5
-curl -o ~/TEMP/PITHWRPIT.mp3 http://www.erh.noaa.gov/er/pbz/crsaudio/PITHWRPIT.mp3 >/dev/null 2>&1
-mpg123 ~/TEMP/PITHWRPIT.mp3 >/dev/null 2>&1
-rm ~/TEMP/PITHWRPIT.mp3
+curl -o ~/TEMP/MIAHWRWPB.mp3 http://www.srh.noaa.gov/images/rtimages/mfl/nwr/audio/MIAHWRWPB.mp3 >/dev/null 2>&1
+mpg123 ~/TEMP/MIAHWRWPB.mp3 >/dev/null 2>&1
+rm ~/TEMP/MIAHWRWPB.mp3
